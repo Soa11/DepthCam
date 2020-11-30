9e92bec8d648a770a0ad5b3b6e8a979ef87b5885
cam <cam.allan.richards@gmail.com>
2020-11-30 12:54:21 +0000

    stash changes

--- Assets/MainThreadController.cs
+++ Assets/MainThreadController.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class MainThreadController : MonoBehaviour
 {
-
+    //This number is being changed by the camera processing blocks.
     public static float average;
 
-    public float seconds = 2f;
+    public float secondsBetweenReports = 2f;
+
+    float minDistance = 100;
+
+    float halfMin;
+
+
 
     float nextTriggerSecond = 0;
+
+
+    float oldAverage;
+
+    float timeInZone = 0;
 
     void Start()
     {
@@ -19,12 +30,47 @@ public class MainThreadController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //check floor
+        if(Time.timeSinceLevelLoad >1 && Time.timeSinceLevelLoad < 5)
+        {
+            if(average < minDistance)
+            {
+                minDistance = average;
+                halfMin = minDistance / 2;
+                Debug.Log("MinDistance= " + minDistance);
+            }
+        }
+
+        if (average < minDistance)
+        {
+            //average is greater than floor
+            timeInZone += 0.01f;
+            Debug.Log("in Zone");
+        }
+        else
+        {
+            timeInZone -= 0.01f;
+            Debug.Log("counting donw");
+
+            if (timeInZone < 0)
+            {
+                timeInZone = 0;
+            }
+        }
+        if(average < halfMin)
+        {
+            Debug.Log("IN HALF DISTANCE");
+        }
+
+
         if (Time.timeSinceLevelLoad > nextTriggerSecond)
         {
-            nextTriggerSecond += seconds;
+
+            nextTriggerSecond += secondsBetweenReports;
 
 
-            Debug.Log("average: " + average);
+            //Debug.Log("average: " + average);
+            oldAverage = average;
         }
     }
 }
